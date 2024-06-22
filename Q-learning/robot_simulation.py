60782f5a44194c8ac640c539d79ed653862c96f1
rengarism <maowhy.1@gmail.com>
2024-06-22 20:16:35 +0700

    renaming

--- Q-learning/robot_simulation.py
+++ Q-learning/robot_simulation.py
@@ -50,8 +50,6 @@ class CrawlingRobotSimulator(object):
         self.display_size = (400, 200)
         self.space = pymunk.Space()
         self.space.gravity = (0.0, -1900.0)
-        # self.space.damping = 0.999 # to prevent it from blowing up.
-        # Pymunk physics coordinates start from the lower right-hand corner of the screen.
         self.ground_y = 100
         self.ground = pymunk.Segment(self.space.static_body, (-100, self.ground_y), (1000, self.ground_y), 5.0)
         self.ground.friction = 1
@@ -69,44 +67,44 @@ class CrawlingRobotSimulator(object):
                     sys.exit(0)
                 if self.control_flag:
                     if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
-                        sim.chassis_b.position = sim.chassisXY
-                        sim.reset_bodies(sim.chassisXY)
+                        sim.body.position = sim.bodyXY
+                        sim.reset_bodies(sim.bodyXY)
                     elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-                        new_angle = sim.motor_ba1Right.angle() + np.deg2rad(40)
-                        sim.motor_ba1Right.set_target_angle(new_angle)
+                        new_angle = sim.motor_hand.angle() + np.deg2rad(40)
+                        sim.motor_hand.set_target_angle(new_angle)
                     elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-                        new_angle = sim.motor_ba1Right.angle() - np.deg2rad(40)
-                        sim.motor_ba1Right.set_target_angle(new_angle)
+                        new_angle = sim.motor_hand.angle() - np.deg2rad(40)
+                        sim.motor_hand.set_target_angle(new_angle)
                     elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
-                        new_angle = sim.motor_ac1Right.angle() - np.deg2rad(20)
-                        sim.motor_ac1Right.set_target_angle(new_angle)
+                        new_angle = sim.motor_arm.angle() - np.deg2rad(20)
+                        sim.motor_arm.set_target_angle(new_angle)
                     elif event.type == pygame.KEYDOWN and event.key == pygame.K_w:
-                        new_angle = sim.motor_ac1Right.angle() + np.deg2rad(20)
-                        sim.motor_ac1Right.set_target_angle(new_angle)
+                        new_angle = sim.motor_arm.angle() + np.deg2rad(20)
+                        sim.motor_arm.set_target_angle(new_angle)
                     elif event.type == pygame.KEYUP:
-                        sim.motor_ac1Right.rate = 0
-                        sim.motor_ba1Right.rate = 0
+                        sim.motor_arm.rate = 0
+                        sim.motor_hand.rate = 0
             for motor in sim.motor_list:
                 motor.update()
             sim.update_simulation()
             sim.draw()
     
     def get_states(self):
-        position_x = self.chassis_b.position[0]
-        ba1Angle = self.motor_ba1Right.angle()
-        ac1Angle = self.motor_ac1Right.angle()
-        return position_x, ba1Angle, ac1Angle
+        position_x = self.body.position[0]
+        handAngle = self.motor_hand.angle()
+        armAngle = self.motor_arm.angle()
+        return position_x, handAngle, armAngle
     
-    def reset_bodies(self,chassisXY):
-        legWd_a = 75
-        legWd_b = 75
+    def reset_bodies(self,bodyXY):
+        armWd_a = 75
+        armWd_b = 75
         chWd = 70
 
-        self.chassis_b.position = chassisXY
-        self.rightLeg_1a_body.position = chassisXY + ((chWd / 2) + (legWd_a / 2), 0)
-        self.rightLeg_1a_body.angle = 0
-        self.rightLeg_1b_body.position = self.rightLeg_1a_body.position + ((legWd_a / 2) + (legWd_b / 2), 0)
-        self.rightLeg_1b_body.angle = 0
+        self.body.position = bodyXY
+        self.arm_body.position = bodyXY + ((chWd / 2) + (armWd_a / 2), 0)
+        self.arm_body.angle = 0
+        self.hand_body.position = self.arm_body.position + ((armWd_a / 2) + (armWd_b / 2), 0)
+        self.hand_body.angle = 0
 
     def draw(self):
         self.screen.fill(THECOLORS["white"])  ### Clear the screen
@@ -126,88 +124,79 @@ class CrawlingRobotSimulator(object):
         self.font = pygame.font.Font(None, 16)
 
         # Create the spider
-        self.chassisXY = Vec2d(20, self.ground_y + 20)
+        self.bodyXY = Vec2d(20, self.ground_y + 20)
         chWd = 70
         chHt = 50
-        chassisMass = 5
+        bodyMass = 5
 
-        legWd_a = 60
-        legHt_a = 8
-        legWd_b = 28
-        legHt_b = 12
-        legMass = 1
+        armWd_a = 60
+        armHt_a = 8
+        armWd_b = 28
+        armHt_b = 12
+        armMass = 1
 
-        self.min_rightLeg_1a_angle = np.deg2rad(-10)
-        self.max_rightLeg_1a_angle = np.deg2rad(10)
-        self.min_rightLeg_1b_angle = np.deg2rad(-140)
-        self.max_rightLeg_1b_angle = np.deg2rad(0)
-        min_rightLeg_1a_angle = self.min_rightLeg_1a_angle
-        max_rightLeg_1a_angle = self.max_rightLeg_1a_angle
-        min_rightLeg_1b_angle = self.min_rightLeg_1b_angle
-        max_rightLeg_1b_angle = self.max_rightLeg_1b_angle
+        self.min_arm_angle = np.deg2rad(-10)
+        self.max_arm_angle = np.deg2rad(10)
+        self.min_hand_angle = np.deg2rad(-140)
+        self.max_hand_angle = np.deg2rad(0)
+        min_arm_angle = self.min_arm_angle
+        max_arm_angle = self.max_arm_angle
+        min_hand_angle = self.min_hand_angle
+        max_hand_angle = self.max_hand_angle
         
         motor_max_force = np.inf
         motor_max_rate = 5
         motor_p_gain = 10
-        # ---chassis
-        self.chassis_b = pymunk.Body(chassisMass, pymunk.moment_for_box(chassisMass, (chWd, chHt)))
-        self.chassis_b.position = self.chassisXY
-        self.chassis_shape = pymunk.Poly.create_box(self.chassis_b, (chWd, chHt))
-        self.chassis_shape.color = 200, 200, 200, 100
-        self.chassis_shape.friction = 0.1
-        print("chassis position")
-        print(self.chassis_b.position)
-        # ---chassis wheel
-        # moment = pymunk.moment_for_circle(10, 1, 5)
-        # cb = pymunk.Body(1, moment)
-        # c = pymunk.Circle(cb, 15)
-        # c.friction = 50
-        # c.elasticity = 0
-        # cb.position = self.chassisXY + Vec2d(-chWd/2+10, -chHt/2+10)
-        # p = pymunk.PinJoint(self.chassis_b, cb, (-chWd/2+10, -chHt/2+10))
-        # self.space.add(cb, c, p)
+        # ---body
+        self.body = pymunk.Body(bodyMass, pymunk.moment_for_box(bodyMass, (chWd, chHt)))
+        self.body.position = self.bodyXY
+        self.body_shape = pymunk.Poly.create_box(self.body, (chWd, chHt))
+        self.body_shape.color = 200, 200, 200, 100
+        self.body_shape.friction = 0.1
+        print("body position")
+        print(self.body.position)
 
-        # ---first right leg a
-        legMass = 0.2
-        self.rightLeg_1a_body = pymunk.Body(legMass, pymunk.moment_for_box(legMass, (legWd_a, legHt_a)))
-        self.rightLeg_1a_body.position = self.chassisXY + ((chWd / 2) + (legWd_a / 2), 0)
-        rightLeg_1a_shape = pymunk.Poly.create_box(self.rightLeg_1a_body, (legWd_a, legHt_a))
-        rightLeg_1a_shape.color = 255, 0, 0, 100
-        rightLeg_1a_shape.friction = 1
-        rightLeg_1a_shape.elasticity = 0
-        # ---first right leg b
-        self.rightLeg_1b_body = pymunk.Body(legMass, pymunk.moment_for_segment(legMass, (0, 0), (legWd_b, 0), 2))
-        self.rightLeg_1b_body.position = self.rightLeg_1a_body.position + ((legWd_a / 2) + (legWd_b / 2), 0)
+        # ---first right arm a
+        armMass = 0.2
+        self.arm_body = pymunk.Body(armMass, pymunk.moment_for_box(armMass, (armWd_a, armHt_a)))
+        self.arm_body.position = self.bodyXY + ((chWd / 2) + (armWd_a / 2), 0)
+        arm_shape = pymunk.Poly.create_box(self.arm_body, (armWd_a, armHt_a))
+        arm_shape.color = 255, 0, 0, 100
+        arm_shape.friction = 1
+        arm_shape.elasticity = 0
+        # ---first right arm b
+        self.hand_body = pymunk.Body(armMass, pymunk.moment_for_segment(armMass, (0, 0), (armWd_b, 0), 2))
+        self.hand_body.position = self.arm_body.position + ((armWd_a / 2) + (armWd_b / 2), 0)
         # Define the start and end points of the segment relative to the body's center
-        start_point = (-legWd_b / 2, 0)
-        end_point = (legWd_b / 2, 0)
-        rightLeg_1b_shape = pymunk.Segment(self.rightLeg_1b_body, start_point, end_point, legHt_b / 2)
-        rightLeg_1b_shape.color = 0, 255, 0, 100
-        rightLeg_1b_shape.friction = 1
-        rightLeg_1b_shape.elasticity = 0
-        # ---link right leg b with right leg a
-        pj_ba1Right = pymunk.PinJoint(self.rightLeg_1b_body, self.rightLeg_1a_body, (-legWd_b / 2, 0),
-                                      (legWd_a / 2, 0))  # anchor point coordinates are wrt the body; not the space
-        self.motor_ba1Right = ServoMotor(self.rightLeg_1b_body, self.rightLeg_1a_body, motor_max_force,motor_max_rate,motor_p_gain)
-        self.motor_ba1Right.min_angle = min_rightLeg_1b_angle
-        self.motor_ba1Right.max_angle = max_rightLeg_1b_angle
-        # ---link right leg a with chassis
-        pj_ac1Right = pymunk.PinJoint(self.rightLeg_1a_body, self.chassis_b, (-legWd_a / 2, 0), (chWd / 2, 0))
-        self.motor_ac1Right = ServoMotor(self.rightLeg_1a_body, self.chassis_b, motor_max_force,motor_max_rate,motor_p_gain)
-        self.motor_ac1Right.min_angle = min_rightLeg_1a_angle
-        self.motor_ac1Right.max_angle = max_rightLeg_1a_angle
-        self.space.add(self.chassis_b, self.chassis_shape)
-        self.space.add(self.rightLeg_1a_body, rightLeg_1a_shape)
-        self.space.add(self.rightLeg_1b_body, rightLeg_1b_shape)
-        # self.space.add(pj_ba1left, motor_ba1Left, pj_ac1left, motor_ac1Left)
-        self.space.add(pj_ba1Right, self.motor_ba1Right, pj_ac1Right, self.motor_ac1Right)
+        start_point = (-armWd_b / 2, 0)
+        end_point = (armWd_b / 2, 0)
+        hand_shape = pymunk.Segment(self.hand_body, start_point, end_point, armHt_b / 2)
+        hand_shape.color = 0, 255, 0, 100
+        hand_shape.friction = 1
+        hand_shape.elasticity = 0
+        # ---link hand with the arm
+        pj_hand = pymunk.PinJoint(self.hand_body, self.arm_body, (-armWd_b / 2, 0),
+                                      (armWd_a / 2, 0))  # anchor point coordinates are wrt the body; not the space
+        self.motor_hand = ServoMotor(self.hand_body, self.arm_body, motor_max_force,motor_max_rate,motor_p_gain)
+        self.motor_hand.min_angle = min_hand_angle
+        self.motor_hand.max_angle = max_hand_angle
+        
+        # ---link arm a with body
+        pj_arm = pymunk.PinJoint(self.arm_body, self.body, (-armWd_a / 2, 0), (chWd / 2, 0))
+        self.motor_arm = ServoMotor(self.arm_body, self.body, motor_max_force,motor_max_rate,motor_p_gain)
+        self.motor_arm.min_angle = min_arm_angle
+        self.motor_arm.max_angle = max_arm_angle
+        self.space.add(self.body, self.body_shape)
+        self.space.add(self.arm_body, arm_shape)
+        self.space.add(self.hand_body, hand_shape)
+        self.space.add(pj_hand, self.motor_hand, pj_arm, self.motor_arm)
 
-        self.motor_list = [self.motor_ba1Right,self.motor_ac1Right]
+        self.motor_list = [self.motor_hand,self.motor_arm]
         # ---prevent collisions with ShapeFilter
         shape_filter = pymunk.ShapeFilter(group=1)
-        self.chassis_shape.filter = shape_filter
-        rightLeg_1a_shape.filter = shape_filter
-        rightLeg_1b_shape.filter = shape_filter
+        self.body_shape.filter = shape_filter
+        arm_shape.filter = shape_filter
+        hand_shape.filter = shape_filter
         # c.filter = shape_filter
 
         # ---collision handler
@@ -218,7 +207,7 @@ class CrawlingRobotSimulator(object):
             self.handContactingGround = False
             return True
         self.ground.collision_type = 1
-        rightLeg_1b_shape.collision_type = 2
+        hand_shape.collision_type = 2
         handler = self.space.add_collision_handler(1, 2)
         handler.begin = on_collision_begin
         handler.separate = on_collision_separate
